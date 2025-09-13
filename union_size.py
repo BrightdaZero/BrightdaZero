1088fce829ac5952f4e9a527703d99e02fd8b098
BrightTzy <nhtoo862005@gmail.com>
2025-09-13 12:00:01 +0700

    Update union_size.py

--- union_size.py
+++ union_size.py
@@ -28,36 +28,3 @@ class DisjointSet:
                 self.parent[rootY] = rootX
                 self.size[rootX] += self.size[rootY]
 
-ds = DisjointSet(12)
-
-ds.union(1, 2)
-ds.union(2, 3)    
-ds.union(4, 5)
-ds.union(6, 7)
-ds.union(5, 6)    
-ds.union(3, 7)  
-
-ds.find(1)
-ds.find(3)
-ds.find(7)
-ds.find(5)
-ds.find(8)
-
-# print("Find 1:", ds.find(1))
-# print("Find 5:", ds.find(5))
-# print("Find 8:", ds.find(8))
-# print("Find 3:", ds.find(3))
-# print("Find 7:", ds.find(7))
-# */
-
-ds.union(1, 7)
-
-# call find on nodes to trigger path compression
-print("Find 1:", ds.find(1))
-print("Find 5:", ds.find(3))
-print("Find 8:", ds.find(7))
-print("Find 3:", ds.find(5))
-print("Find 7:", ds.find(8))
-
-print("Parent:", ds.parent)
-print("Size:  ", ds.size)
